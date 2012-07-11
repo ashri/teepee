05e5b90ae2b7006bf078ce1141509082f2184aec
Ashley Richardson <ash@threeheadedmonkey.com>
2012-07-11 22:24:10 +0800

    Adding Consolidator with stub implementation

--- src/main/java/com/threeheadedmonkey/teepee/entity/Consolidator.java
+++ src/main/java/com/threeheadedmonkey/teepee/entity/Consolidator.java
@@ -1,11 +1,30 @@
 package com.threeheadedmonkey.teepee.entity;
 
+import java.util.Collection;
+
 /**
- * Created with IntelliJ IDEA.
- * User: ashley
- * Date: 11/07/12
- * Time: 9:21 PM
- * To change this template use File | Settings | File Templates.
+ * Consolidate a list of flat items into a project/task/note hierarchy
  */
 public class Consolidator {
+
+    private final Collection<Item> items;
+
+
+    /**
+     * Construct Consolidator with provided items
+     *
+     * @param items the collection of items to consolidate
+     */
+    public Consolidator(Collection<Item> items) {
+        this.items = items;
+    }
+
+    /**
+     * Consolidate a collection of flat items into a tree of items where Tasks fall under Projects and so on
+     *
+     * @return the tree of items
+     */
+    public Collection<Item> consolidate() {
+        return items;
+    }
 }
--- src/test/java/com/threeheadedmonkey/teepee/entity/ConsolidatorTest.java
+++ src/test/java/com/threeheadedmonkey/teepee/entity/ConsolidatorTest.java
@@ -3,21 +3,49 @@ package com.threeheadedmonkey.teepee.entity;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 /**
- * Created with IntelliJ IDEA.
- * User: ashley
- * Date: 11/07/12
- * Time: 9:23 PM
- * To change this template use File | Settings | File Templates.
+ * Unit test for the Consolidator
  */
 public class ConsolidatorTest {
+
+    private Collection<Item> items;
+
     @Before
     public void setUp() throws Exception {
-
+        items = new ArrayList<Item>();
+        items.add(new Project("Project 1"));
+        items.add(new Task("Task 1"));
+        items.add(new Project("Project 2"));
+        items.add(new Task("Task 2"));
+        items.add(new Task("Note 1"));
+        items.add(new Task("Task 3"));
+        items.add(new Project("Project 3"));
+        items.add(new Task("Task 4"));
     }
 
     @Test
     public void testConsolidate() throws Exception {
+        Collection<Item> consolidatedItems = new Consolidator(items).consolidate();
+        assertNotNull(consolidatedItems);
+        assertEquals(3, consolidatedItems.size());
+
+        Iterator<Item> itIt = consolidatedItems.iterator();
+
+        Item i0 = itIt.next();
+        assertEquals(Project.class, i0.getClass());
+
+        Item i1 = itIt.next();
+        assertEquals(Project.class, i1.getClass());
+
+        Item i2 = itIt.next();
+        assertEquals(Project.class, i2.getClass());
 
     }
 }
